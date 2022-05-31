3faba4e59da4c272f613b7c3f7b4ef353f2bbd25
Kitkath19 <67288052+Kitkath19@users.noreply.github.com>
2022-06-01 00:51:20 +0800

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,65 +1,76 @@
-import math
-import time
-import requests
 import socket
+import sys
 import argparse
-import hashlib
-
-PID = "2099fba5"                # Personal ID here
-SENDER_PORT = 6679      # Port number used by the sender
 
 
-def parseArguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", type=str,
-                        help="File to send", default=f"{PID}.txt")
-    parser.add_argument("-a", "--address", type=str,
-                        help="Server IP address",  default="10.0.7.141")
-    parser.add_argument("-s", "--receiver_port", type=int,
-                        help="Port number used by the receiver", default=SENDER_PORT)
-    parser.add_argument("-c", "--sender_port", type=int,
-                        help="Port number used by the sender", default=9000)
-    parser.add_argument("-i", "--id", type=str,
-                        help="Unique Identifier", default=PID)
-    parser.add_argument("-t", "--testcases", type=int,
-                        help="Number of Testcases", default=1)
-    args = parser.parse_args()
-    return args
+# STEP 0: Getting the Command Line Input
+# angparse was used so that there can be blank parts
+# blank parts will be replaced by the default value/s
+# declaration of the use of parser
+parser = argparse.ArgumentParser(description='Project Input Parameters.')
+# adding of argument 1: -f denoting Filename of the Payload
+# type is str
+# default value is your_id.txt in my case your_id = 2099fba5
+parser.add_argument("-f", "--filename_payload", type=str,
+                    help="Filename of the Payload", default=f"2099fba5.txt")
+# adding of argument 2: -a denoting IP address of the Receiver
+# type is str
+# default value is 10.0.7.141 (given in project specifications)                   
+parser.add_argument("-a", "--IP_address", type=str,
+                    help="IP address of the Receiver",  default="10.0.7.141")
+# adding of argument 3: -s denoting Port number used by the receiver
+# type is int
+# default value is is your assigned port, in my case is 6679                    
+parser.add_argument("-s", "--port_receiver", type=int,
+                    help="Port number used by the receiver", default=6679)
+# adding of argument 4: -c denoting Port number used by the sender
+# type is int
+# default value is 9000 (given in project specifications)                   
+parser.add_argument("-c", "--port_sender", type=int,
+                    help="Port number used by the sender", default=9000)
+# adding of argument 5: -i denoting Unique ID
+# type is str
+# default value is your_id.txt in my case your_id = 2099fba5                    
+parser.add_argument("-i", "--unique_ID", type=str,
+                    help="Unique ID", default='2099fba5')
+args = parser.parse_args()
+
+'''
+print(args.payload)
+print(args.IP_address)
+print(args.port_receiver)
+print(args.port_sender)
+print(args.id)
+'''
+
+# STEP 1: Dowloading Payload
+# Transaction Generation/Results Server (TGRS)
+# http://3.0.248.41:5000/get_data?student_id = wwwwwwww
+# wwwwwww is the unique ID given in the email
+# unique ID given/default: 2099fba5
+# TGRS = f"http://3.0.248.41:5000/get_data?student_id={args.unique_ID}"
+# note that: this is NOT needed since the payload must be uploaded before running the program
+# as stated in the project specifications
 
 
-class Sender:
-    def __init__(self, args) -> None:
-        self.PID = PID
-        self.SENDER_PORT = SENDER_PORT
-        self.FILE_NAME = args.file
-        self.SENDER_PORT_NO = args.sender_port
-        self.RECEIVER_PORT_NO = args.receiver_port
-        self.IP_ADDRESS = args.address
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('', self.RECEIVER_PORT_NO))
-
-    # Downloading the payload
-    def downloadPackage(self):
-        URL = f"http://3.0.248.41:5000/get_data?student_id={self.PID}"
-        response = requests.get(URL)
-        open(self.FILE_NAME, "wb").write(response.content)
-
-    # Sending an Intent Message
-    def sendIntentMessage(self):
-        self.timer = time.time()
-        intent = f"ID{self.PID}".encode()
-        self.sock.sendto(intent, (self.IP_ADDRESS, self.SENDER_PORT_NO))
-        data, _ = self.sock.recvfrom(self.RECEIVER_PORT_NO)
-        self.TID = data.decode()
-
-
-args = parseArguments()
-sender = Sender(args)
-sender.downloadPackage()
-sender.sendIntentMessage()
-if sender.TID != "Existing alive transaction":
-    sender.sendPackage()
-    sender.waitEnd()
-    sender.log()
-else:
-    print("Existing alive transaction")
+# STEP 2: Initiating a Transaction
+# 2.1   Intent Message IDwwwwwwww
+# wwwwwwww is the unique ID given in the email
+# default unique_ID = "2099fba5"
+# setting up the intent message of format : ID + unique_iID
+intent_message = ("ID" + args.unique_ID).encode()
+# 2.2   Accept Message YYYYYYY
+# accept message will be printed out once it is proven 
+# that there is no alive transaction after doing 2.1
+# YYYYYYY is the transaction id that allows the user to check if the transmission is valid
+#socket initialization
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# bind socket address to port receiver
+sock.bind(('', args.port_receiver))
+# using the intent message from 2.1 send data to address
+sock.sendto(intent_message, (args.IP_address, args.port_sender))
+# store the acknowledgement number from port
+acknowledgement,_ = sock.recvfrom(args.port_receiver)
+# decode acknowledgement number
+trasaction_ID = acknowledgement.decode()
+print(trasaction_ID)
