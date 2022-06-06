5e9ed477ce9febe34606dd4b1b94ecfd1be18069
Kitkath19 <67288052+Kitkath19@users.noreply.github.com>
2022-06-06 11:48:13 +0800

    Update sender.py

--- sender.py
+++ sender.py
@@ -138,7 +138,7 @@ def STEP_3_3():
             else:
                 limitation = original 
 
-            payload_size = max(payload_size - 1, last_accepted_payload_size)
+            payload_size = max((1 + payload_size) /2, last_accepted_payload_size)
             # repeat setep 3_3
             return STEP_3_3()
 
