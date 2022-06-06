79366117036ba503ab8592943baf2c6fe4bc61a1
Kitkath19 <67288052+Kitkath19@users.noreply.github.com>
2022-06-06 17:01:47 +0800

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,22 +1,79 @@
 import socket
+import sys
 import argparse
 import time
 import math
 
 
+# the formula for this function was retrieved from lecture 11
+def RTT_estimation():
+# declaration of global variables
+    global EstimatedRTT, DevRTT, TimeoutInterval, SampleRTT
+    # part 1 - estimated RTT computation
+    # check if this is the initial transactiom
+    # if it is the initial
+    if EstimatedRTT == 0:
+    # set EstimatedRTT = RTT
+    # the RTT was computed previously  RTT = (RTT_end_time - RTT_start_time)
+        EstimatedRTT = SampleRTT
+    # if it is not the initial
+    else:
+    # Lecture 11 page 18
+    # “Average” of SampleRTT values must be taken; in TCP, this is called EstimatedRTT
+    # EstimatedRTT updated for each new value of SampleRTT
+        EstimatedRTT = (1 - 0.125) * EstimatedRTT + (0.125 * SampleRTT)
+
+    # part 2 - dev RTT computation
+    # Lecture 11 page 19
+    # DevRTT - estimate of how much SampleRTT typically varies from EstimatedRTT
+    DevRTT = (1 - 0.25) * DevRTT + 0.25 * abs(SampleRTT - EstimatedRTT)
+
+    # part 3 - rate update
+    # Lecture 11 page 20
+    # From the EstimatedRTT and DevRTT, the timeout interval is derived
+    TimeoutInterval = EstimatedRTT + (4 * DevRTT)
+
+    
+
+
+
+def PARAMETER_estimation():
+    # declaration of global variables
+    global time_elapsed, remaining_size, payload_size, target_time, time_taken, remaining_packets, TimeoutInterval
+    global last_accepted_payload_size, limitation, sent_packets, original
+
+    sent_packets += payload_size
+    last_accepted_payload_size = payload_size
+        #run += 1
+
+    time_elapsed = time.time() - start_time
+    target_time = target_time if time_elapsed < target_time else 120
+    rem_data = original - sent_packets
+    rem_packets = math.ceil(rem_data / payload_size)
+    time_taken = time_elapsed + (rem_packets * TimeoutInterval)
+
+    rem_time = target_time - time_elapsed
+    rem_data = original - sent_packets
+    rem_packets = math.floor(rem_time/TimeoutInterval)
+    if time_taken > target_time:
+        payload_size = max( math.ceil(rem_data / rem_packets), last_accepted_payload_size + 1 )
+        payload_size = payload_size if payload_size < limitation else limitation - 1
+
+
 # Step 3.3: Continuing the program
 # function was used to make the code faster
-def STEP_3():
-    # declaration of global variables to be used in this function
+# number of runs done
+run = 0
+def STEP_3_3():
     global payload_size, remaining_size, TimeoutInterval, payload, remaining_packets, start_time, run, SampleRTT
     global last_accepted_payload_size, time_taken, limitation, time_elapsed, original, sent_packets
-    global EstimatedRTT, DevRTT, TimeoutInterval, SampleRTT
-    # checking if the length of sent packets is less than the original packet length
     while sent_packets <= original:
-
-
         # separating the contents -> list format
         separated_payload = payload[sent_packets: sent_packets + int(payload_size)]
+        # print(separated_payload)
+        # sending of details to server
+
+        #print(separated_payload[i])
         # sequence_number = SNXXXXXXX
         # always starts at 0
         sequence_number = str(run)
@@ -39,7 +96,6 @@ def STEP_3():
         # using the intent message from 2.1 send data to address
         sock.sendto(data_packet, (args.IP_address, args.port_receiver))
 
-        # run when socket does NOT time out
         try:
             # timer for start of initiation
             RTT_start_time = time.time() 
@@ -54,98 +110,36 @@ def STEP_3():
             RTT_end_time = time.time()
             # computing for the payload size (RTT)
             SampleRTT = (RTT_end_time - RTT_start_time)
-
-            # 3.1   RTT Estimation
-            # run RTT estimation
-
-            # part 1 - estimated RTT computation
-            # check if this is the initial transactiom
-            # if it is the initial
-            if EstimatedRTT == 0:
-            # set EstimatedRTT = RTT
-            # the RTT was computed previously  RTT = (RTT_end_time - RTT_start_time)
-                EstimatedRTT = SampleRTT
-            # if it is not the initial
-            else:
-            # Lecture 11 page 18
-            # “Average” of SampleRTT values must be taken; in TCP, this is called EstimatedRTT
-            # EstimatedRTT updated for each new value of SampleRTT
-                EstimatedRTT = (1 - 0.125) * EstimatedRTT + 0.125 * SampleRTT
-
-            # part 2 - dev RTT computation
-            # Lecture 11 page 19
-            # DevRTT - estimate of how much SampleRTT typically varies from EstimatedRTT
-            DevRTT = (1 - 0.25) * DevRTT + 0.25 * abs(SampleRTT - EstimatedRTT)
-
-            # part 3 - rate update
-            # Lecture 11 page 20
-            # From the EstimatedRTT and DevRTT, the timeout interval is derived
-            TimeoutInterval = EstimatedRTT + (4 * DevRTT)
-
+            RTT_estimation()
             # timeout interval will be used in settimeout for each packet
             if TimeoutInterval != 0:
                 sock.settimeout(math.ceil(TimeoutInterval))
                 
-
             # for each successful upload run is incremented
             run += 1
-            # updating the total of send packets
-            sent_packets = sent_packets + payload_size
-            # setting the last_accepted_payload_size to the payload_size
-            last_accepted_payload_size = payload_size
 
-
-            # 3.2   Parameter Estimation
-            # time spent since the start of initiation
-            time_elapsed = time.time() - start_time
-            # time left to send the remaining payload
-            rem_time = target_time - time_elapsed
-            # compute for the remaining data to be sent
-            rem_data = original - sent_packets
-            # divide the data into packets
-            rem_packets = math.ceil(rem_data / payload_size)
-            # compute for the estimate time it will take
-            time_taken = time_elapsed + (rem_packets * TimeoutInterval)
-            # divide the data into packets
-            rem_packets = math.floor(rem_time / TimeoutInterval)
-            # chceking if it will exceed the target time of 95s
-            # if it is greater
-            if time_taken > target_time:
-            # checking which size is greater so that it can try to reach the target time
-                payload_size = max( math.ceil(rem_data / rem_packets), last_accepted_payload_size + 1 )
-                # checking if the size is greater than the limit set previously
-                # if it is less
-                if payload_size < limitation:
-                # set the current payload size as the size
-                    payload_size = payload_size 
-                # if it is more
-                else:
-                # lessen the limit and set it to the payload size so it can reach the target time
-                    payload_size = limitation - 1
+            # update remaining size
+            PARAMETER_estimation()
+            # print(remaining_size)
             
 
-        # when the socket time outs
         except socket.timeout:
             # remaining packets to be sent
             # remaining_packets = (95 - time_elapsed) / TimeoutInterval
             remaining_packets = math.ceil((original - sent_packets) / payload_size)
             # computing for time taken
             time_taken = (remaining_packets * TimeoutInterval) + (TimeoutInterval + time_elapsed)
-            
-            # getting the boundaries of the payload
-            if payload_size != last_accepted_payload_size:
-            # setting the uper bound to the payload size
-                limitation = payload_size
-            # setting the uper bound to the total payload size
-            else:
-                limitation = original
 
-            # computing which payload is bigger
-            # using the payload in the next run
+            limitation = payload_size if payload_size != last_accepted_payload_size else original
             payload_size = max(payload_size - 1, last_accepted_payload_size)
-            # repeat setep 3
-            return STEP_3()
+            # repeat setep 3_3
+            return STEP_3_3()
 
+
+
+
+#declaration of variables that will be used throughout the code
+#global filename_payload, IP_address, port_receiver, port_sender, unique_ID, trasaction_ID, intent_message
 
 
 # STEP 0: Getting the Command Line Input
@@ -181,6 +175,7 @@ parser.add_argument("-i", "--unique_ID", type=str,
 args = parser.parse_args()
 
 
+
 # STEP 1: Dowloading Payload
 # Transaction Generation/Results Server (TGRS)
 # http://3.0.248.41:5000/get_data?student_id = wwwwwwww
@@ -191,15 +186,12 @@ args = parser.parse_args()
 # as stated in the project specifications
 
 
-
 # STEP 2: Initiating a Transaction
-
 # 2.1   Intent Message IDwwwwwwww
 # wwwwwwww is the unique ID given in the email
 # default unique_ID = "2099fba5"
 # setting up the intent message of format : ID + unique_iID
 intent_message = f"ID{args.unique_ID}".encode()
-
 # 2.2   Accept Message YYYYYYY
 # accept message will be printed out once it is proven 
 # that there is no alive transaction after doing 2.1
@@ -218,16 +210,17 @@ start_time = time.time()
 trasaction_ID = acknowledgement.decode()
 print(trasaction_ID)
 
-# 2.3   Checking if there is an existing transaction
+
+
+# Step 2.5: Checking if there is an existing transaction
 # check if transaction ID says that there is an alive tranaction
 if trasaction_ID == "Existing alive transaction":
 # if there is print Existing alive transaction 
     print(trasaction_ID)
 # if no live tranaction
+# continue on step 3
 else:
-# Step 3: Sending the Payload
-
-    # sending the Payload
+    # Step 3: Sending the Payload
     # sending the data packets
     # intent_message = IDWWWWWWWW
     # retrieve intent_message from PART 2 (un)code it
@@ -242,38 +235,28 @@ else:
     payload = str(file.read())
     payload = payload.rstrip()
     
-    # RTT_estimation variables
+    #print(RTT)
     # set initial EstimatedRTT to 0
     # since no transaction/runs were made
-    # computes for the EstimatedRTT
     EstimatedRTT = 0
     # set initial TimeoutInterval to 0
     # since no transaction/runs were made
-    # computes for the TimeoutInterval
     TimeoutInterval = 0
     # set initial DevRTT to 0
     # since no transaction/runs were made
-    # computes for the DevRTT
     DevRTT = 0
     
-
-    # PARAMETER_estimation variables
-    
     remaining_size = len(payload)
-    original = len(payload)
-    limitation = original
     remaining_packets = 0
     time_taken = 0
     last_accepted_payload_size = 0
     payload_size = 1
+    
     time_elapsed = 0
     target_time = 95
     sent_packets = 0
+    original = len(payload)
+    limitation = original
 
-
-    # STEP_3 variables
-    # set run to 0
-    # since no transaction/runs were made
-    # computes for the total number of runs made
-    run = 0
-    STEP_3()
+   
+    STEP_3_3()
