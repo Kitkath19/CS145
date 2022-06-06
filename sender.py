4dc152d3f9cf311a7a7fa05cfe0b2ea64a2de8cc
Kitkath19 <67288052+Kitkath19@users.noreply.github.com>
2022-06-06 15:03:07 +0800

    Update sender.py

--- sender.py
+++ sender.py
@@ -78,7 +78,7 @@ def STEP_3_3():
         # always starts at 0
         sequence_number = str(run)
         # checking if last payload
-        if sent_packets == len(payload) - 1:
+        if remaining_size == 0:
             # transmission_number = LASTZ
             # 0 if not the last
             # 1 if the last
